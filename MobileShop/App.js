1a0288aa54e60e96284e9dedee6b81f45a936f06
USER <jeffkalbeni@gmail.com>
2024-07-12 23:51:18 +0000

    This is the entry point to the appliaction

---
+++ MobileShop/App.js
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Cart from './components/CartScreen';
+import HomeScreen from './components/HomeScreen';
+import ProductDetail from './components/ProductDetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Product Detail" component={ProductDetail}/>
+        <Stack.Screen name="Cart" component={Cart}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
